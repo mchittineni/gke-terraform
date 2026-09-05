bb3c33fdf91963a86907cf0bf754766997deb7c4
mchittineni <manideep.chittineni@hotmail.com>
2026-09-05 14:24:45 +0100

    refactor(networking): remove unused variables and module arguments

--- modules/gcp/networking/variables.tf
+++ modules/gcp/networking/variables.tf
@@ -17,19 +17,3 @@ variable "subnet_cidr" {
   description = "CIDR range for the primary subnet"
   type        = string
 }
-
-variable "environment" {
-  description = "Deployment environment"
-  type        = string
-}
-
-variable "project_name" {
-  description = "Project label for tagging"
-  type        = string
-}
-
-variable "labels" {
-  description = "Additional labels applied to resources"
-  type        = map(string)
-  default     = {}
-}
