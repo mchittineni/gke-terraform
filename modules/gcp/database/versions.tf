890a898da9492a727bdb2b08a5c6d7197bbfeb7b
mchittineni <manideep.chittineni@hotmail.com>
2026-09-05 14:23:21 +0100

    refactor(modules): add versions.tf to child modules and remove unused locals

---
+++ modules/gcp/database/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.15.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 6.20.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.9.0"
+    }
+  }
+}
